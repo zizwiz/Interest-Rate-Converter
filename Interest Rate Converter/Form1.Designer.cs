4f171e4e44afda93a3683784f66ba08e773276ef
ziz wiz <zizwiz@engineer.com>
2024-04-19 12:07:10 +0100

    Completed AER to Gross

--- Interest Rate Converter/Form1.Designer.cs
+++ Interest Rate Converter/Form1.Designer.cs
@@ -33,16 +33,23 @@ namespace Interest_Rate_Converter
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tab_AerToGross = new System.Windows.Forms.TabPage();
-            this.tab_GrossToAER = new System.Windows.Forms.TabPage();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.btn_calc = new System.Windows.Forms.Button();
-            this.btn_close = new System.Windows.Forms.Button();
-            this.btn_reset = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.tab_GrossToAER = new System.Windows.Forms.TabPage();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_calc = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.cmbobx_interest_frequency = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbx_GrossInterest = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_AerToGross = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tab_AerToGross.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.tab_GrossToAER.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
@@ -59,7 +66,7 @@ namespace Interest_Rate_Converter
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(892, 590);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // tabControl1
@@ -70,7 +77,7 @@ namespace Interest_Rate_Converter
             this.tabControl1.Location = new System.Drawing.Point(3, 3);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(794, 364);
+            this.tabControl1.Size = new System.Drawing.Size(886, 504);
             this.tabControl1.TabIndex = 0;
             // 
             // tab_AerToGross
@@ -79,10 +86,24 @@ namespace Interest_Rate_Converter
             this.tab_AerToGross.Location = new System.Drawing.Point(4, 29);
             this.tab_AerToGross.Name = "tab_AerToGross";
             this.tab_AerToGross.Padding = new System.Windows.Forms.Padding(3);
-            this.tab_AerToGross.Size = new System.Drawing.Size(786, 331);
+            this.tab_AerToGross.Size = new System.Drawing.Size(878, 471);
             this.tab_AerToGross.TabIndex = 0;
             this.tab_AerToGross.Text = "AER to Gross";
             this.tab_AerToGross.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl_AerToGross);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtbx_GrossInterest);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.cmbobx_interest_frequency);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(872, 465);
+            this.panel1.TabIndex = 0;
             // 
             // tab_GrossToAER
             // 
@@ -94,6 +115,14 @@ namespace Interest_Rate_Converter
             this.tab_GrossToAER.TabIndex = 1;
             this.tab_GrossToAER.Text = "Gross to AER";
             this.tab_GrossToAER.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(780, 325);
+            this.panel2.TabIndex = 0;
             // 
             // tableLayoutPanel2
             // 
@@ -107,34 +136,34 @@ namespace Interest_Rate_Converter
             this.tableLayoutPanel2.Controls.Add(this.btn_calc, 2, 0);
             this.tableLayoutPanel2.Controls.Add(this.btn_reset, 0, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 373);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 513);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(794, 74);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(886, 74);
             this.tableLayoutPanel2.TabIndex = 1;
-            // 
-            // btn_calc
-            // 
-            this.btn_calc.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn_calc.Location = new System.Drawing.Point(300, 3);
-            this.btn_calc.Name = "btn_calc";
-            this.btn_calc.Size = new System.Drawing.Size(194, 68);
-            this.btn_calc.TabIndex = 0;
-            this.btn_calc.Text = "Calculate";
-            this.btn_calc.UseVisualStyleBackColor = true;
-            this.btn_calc.Click += new System.EventHandler(this.btn_calc_Click);
             // 
             // btn_close
             // 
             this.btn_close.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn_close.Location = new System.Drawing.Point(597, 3);
+            this.btn_close.Location = new System.Drawing.Point(689, 3);
             this.btn_close.Name = "btn_close";
             this.btn_close.Size = new System.Drawing.Size(194, 68);
             this.btn_close.TabIndex = 1;
             this.btn_close.Text = "Close";
             this.btn_close.UseVisualStyleBackColor = true;
             this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // btn_calc
+            // 
+            this.btn_calc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_calc.Location = new System.Drawing.Point(346, 3);
+            this.btn_calc.Name = "btn_calc";
+            this.btn_calc.Size = new System.Drawing.Size(194, 68);
+            this.btn_calc.TabIndex = 0;
+            this.btn_calc.Text = "Calculate";
+            this.btn_calc.UseVisualStyleBackColor = true;
+            this.btn_calc.Click += new System.EventHandler(this.btn_calc_Click);
             // 
             // btn_reset
             // 
@@ -147,27 +176,71 @@ namespace Interest_Rate_Converter
             this.btn_reset.UseVisualStyleBackColor = true;
             this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
             // 
-            // panel1
+            // cmbobx_interest_frequency
             // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(3, 3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(780, 325);
-            this.panel1.TabIndex = 0;
+            this.cmbobx_interest_frequency.FormattingEnabled = true;
+            this.cmbobx_interest_frequency.Items.AddRange(new object[] {
+            "Annual",
+            "Bi-Annual",
+            "Quarterly",
+            "Bi-Monthly",
+            "Monthly",
+            "Bi-Weekly",
+            "Weekly",
+            "Daily"});
+            this.cmbobx_interest_frequency.Location = new System.Drawing.Point(180, 89);
+            this.cmbobx_interest_frequency.Name = "cmbobx_interest_frequency";
+            this.cmbobx_interest_frequency.Size = new System.Drawing.Size(174, 28);
+            this.cmbobx_interest_frequency.TabIndex = 0;
             // 
-            // panel2
+            // label1
             // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(3, 3);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(780, 325);
-            this.panel2.TabIndex = 0;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Interest Frequency";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Gross Interest Rate";
+            // 
+            // txtbx_GrossInterest
+            // 
+            this.txtbx_GrossInterest.Location = new System.Drawing.Point(180, 40);
+            this.txtbx_GrossInterest.Name = "txtbx_GrossInterest";
+            this.txtbx_GrossInterest.Size = new System.Drawing.Size(112, 26);
+            this.txtbx_GrossInterest.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(298, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "%";
+            // 
+            // lbl_AerToGross
+            // 
+            this.lbl_AerToGross.AutoSize = true;
+            this.lbl_AerToGross.Location = new System.Drawing.Point(24, 207);
+            this.lbl_AerToGross.Name = "lbl_AerToGross";
+            this.lbl_AerToGross.Size = new System.Drawing.Size(21, 20);
+            this.lbl_AerToGross.TabIndex = 5;
+            this.lbl_AerToGross.Text = "...";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(892, 590);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
@@ -176,6 +249,8 @@ namespace Interest_Rate_Converter
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tabControl1.ResumeLayout(false);
             this.tab_AerToGross.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.tab_GrossToAER.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -194,6 +269,12 @@ namespace Interest_Rate_Converter
         private System.Windows.Forms.Button btn_close;
         private System.Windows.Forms.Button btn_calc;
         private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbobx_interest_frequency;
+        private System.Windows.Forms.Label lbl_AerToGross;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbx_GrossInterest;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Interest Rate Converter/Form1.cs
+++ Interest Rate Converter/Form1.cs
@@ -15,6 +15,7 @@ namespace Interest_Rate_Converter
         {
             Text += " : v" +
                     Assembly.GetExecutingAssembly().GetName().Version; // put in the version number
+            cmbobx_interest_frequency.SelectedIndex = 0;
         }
 
         private void btn_close_Click(object sender, EventArgs e)
@@ -26,7 +27,9 @@ namespace Interest_Rate_Converter
         {
             if (tabControl1.SelectedTab == tab_AerToGross)
             {
-
+                txtbx_GrossInterest.Text = "";
+                lbl_AerToGross.Text = "";
+                cmbobx_interest_frequency.SelectedIndex = 0;
             }
             else
             {
@@ -38,12 +41,41 @@ namespace Interest_Rate_Converter
         {
             if (tabControl1.SelectedTab == tab_AerToGross)
             {
+                ///AER = (1 + r/n)^n – 1
+                /// r is the gross interest in decimal 5% = 0.05 
+                ///n is the number of compounding periods in any 12 - month period.
+                /// daily is for next twelve months beware of leap year as this adds extra day.
+                
+                double InterestFrequency;
 
+                switch (cmbobx_interest_frequency.Text)
+                {
+                    case "Annual": InterestFrequency = 12; break;
+                    case "Bi-Annual": InterestFrequency = 6; break;
+                    case "Quarterly": InterestFrequency = 4; break;
+                    case "Bi-Monthly": InterestFrequency = 24; break;
+                    case "Monthly": InterestFrequency = 12; break;
+                    case "Bi-Weekly": InterestFrequency = 104; break;
+                    case "Weekly": InterestFrequency = 52; break;
+                    case "Daily": InterestFrequency = NumberOfDaysInNextYear(); break;
+                    default: InterestFrequency = 0; break;
+                }
+               
+                lbl_AerToGross.Text = "AER interest for 1 year from today = " + Math.Round(((Math.Pow((1 + ((double.Parse(txtbx_GrossInterest.Text) / 100) / InterestFrequency)), InterestFrequency)-1)*100),2);
             }
             else
             {
 
             }
+        }
+
+        private int NumberOfDaysInNextYear()
+        {
+            var futureDate = new DateTime(DateTime.Now.Year + 1, DateTime.Now.Month, DateTime.Now.Day);
+            var currentDate = DateTime.Today;
+
+            TimeSpan daysInNextYear = futureDate - currentDate;
+            return daysInNextYear.Days;
         }
     }
 }
